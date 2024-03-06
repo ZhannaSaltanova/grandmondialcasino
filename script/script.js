bf09556b1e14c3bac74c0f39f47f61ce61e5a553
ZhannaSaltanova <nedikovazhanna@gmail.com>
2024-03-06 14:01:35 +0200

    table

--- script/script.js
+++ script/script.js
@@ -39,8 +39,4 @@ const swiper = new Swiper('.swiper', {
   $('.burger-button').on('click', function(e) {
     e.preventDefault();  
     $('.burgermenu').toggleClass('burgermenu_active');
-   })
-  $('.burger-list a').click(function(e) {
-    e.preventDefault();  
-     $('.burgermenu').toggleClass('burgermenu_active');
-     })
+   });
