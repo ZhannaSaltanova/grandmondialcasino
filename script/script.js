e789f2957ba0b365434449e88697a6cb181c0e19
ZhannaSaltanova <nedikovazhanna@gmail.com>
2024-03-07 07:21:35 +0200

    fix

--- script/script.js
+++ script/script.js
@@ -36,7 +36,14 @@ const swiper = new Swiper('.swiper', {
   });
 
 
-  $('.burger-button').on('click', function(e) {
-    e.preventDefault();  
-    $('.burgermenu').toggleClass('burgermenu_active');
-   });
+  $(document).ready(function() {
+    $('.burger-button').on('click', function(e) {
+      e.preventDefault();  
+      $('.burgermenu').toggleClass('burgermenu_active');
+    });
+  
+    $('.close').on('click', function(e) {
+      e.preventDefault();
+      $('.burgermenu').removeClass('burgermenu_active');
+    });
+  });
